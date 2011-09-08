0b6da5d6f1adbeb3ed82ef9b0e973eae8e06ceff
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-09-09 00:27:06 +0200

    Added a twitter search in PHP - untested.

---
+++ modules/twitterdemo/files/phpdemo/index.php
@@ -0,0 +1,24 @@
+<form action="index.php" method="submit">
+<input name="twitterq" type="text" id="twitterq" />
+<input name="Search" type="submit" value="Search" />
+</form>
+<?php 
+include('TwitterSearch.php');
+if($_GET['twitterq']){
+$twitter_query = $_GET['twitterq'];
+$search = new TwitterSearch($twitter_query);
+$results = $search->results();
+
+foreach($results as $result){
+echo '<div class="twitter_status">';
+echo '<img src="'.$result->profile_image_url.'" class="twitter_image">';
+$text_n = toLink($result->text);
+echo $text_n;
+echo '<div class="twitter_small">';
+echo '<strong>From:</strong> <a href="http://www.twitter.com/'.$result->from_user.'">'.$result->from_user.'</a>: ';
+echo '<strong>at:</strong> '.$result->created_at;
+echo '</div>';
+echo '</div>';
+}
+}
+?>
